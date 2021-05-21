34715a87a725d0216f5ab9d0c657ba086efeea08
Harris201018 <76244880+Harris201018@users.noreply.github.com>
2021-05-21 13:08:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//ODQwODc4OTI2NTE4NjgxNjAw.YJenhg.y4rTTqXoG6LtSXv9PMKknlYQPEw
+const Discord = require('discord.js')
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('active!')
+})
+
+
+
+client.login('ODQwODc4OTI2NTE4NjgxNjAw.YJenhg.y4rTTqXoG6LtSXv9PMKknlYQPEw')
